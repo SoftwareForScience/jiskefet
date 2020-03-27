78390a464d61d9f110063a5e2261ffa40ec91b41
John Ghatas <johnghatas97@hotmail.com>
2020-03-27 16:36:06 +0100

    Added the table to the homepage in page.js. Created a general overview model for use in the detailed view and in the general overview

---
+++ frontend/lib/public/views/Overview/General/page.js
@@ -0,0 +1,21 @@
+import { h } from "/js/src/index.js";
+
+export default model => [dataTable(model)];
+
+const dataTable = model => {
+  const headers = model.overview.getHeaders();
+  console.log(
+    headers.map(header => {
+      return rowHeader(header);
+    })
+  );
+  return h("table.table.shadow-level2", [
+    h("tr", [
+      headers.map(header => {
+        return rowHeader(header);
+      })
+    ])
+  ]);
+};
+
+const rowHeader = header => [h("th", header)];
