ee0523746e11453179dc0fdaced2f331796eeea8
John Ghatas <johnghatas97@hotmail.com>
2020-03-31 23:07:12 +0200

    feat: Got filtering working, reflects on the actual table now

--- frontend/lib/public/views/Overview/General/page.js
+++ frontend/lib/public/views/Overview/General/page.js
@@ -1,6 +1,6 @@
-import { h } from '/js/src/index.js';
-import filters from '../../../components/Filters/index.js';
-import { rowData, rowHeader } from '../../../components/Table/index.js';
+import { h } from "/js/src/index.js";
+import filters from "../../../components/Filters/index.js";
+import { rowData, rowHeader } from "../../../components/Table/index.js";
 
 export default model => [overviewScreen(model)];
 
@@ -9,16 +9,16 @@ const overviewScreen = model => {
   const data = model.overview.getTableData();
   const tags = model.overview.getTagCounts();
 
-  return h('.w-75.flex-row', [
-    filters(tags),
-    h('table.table.shadow-level1.mh3', [
-      h('tr', [
+  return h(".w-75.flex-row", [
+    filters(model, tags),
+    h("table.table.shadow-level1.mh3", [
+      h("tr", [
         headers.map(header => {
           return rowHeader(header);
         })
       ]),
       data.map((entry, index) => {
-        return h('tr', [
+        return h("tr", [
           rowData(index + 1),
           Object.keys(entry).map(subitem => {
             return rowData(entry[subitem]);
--- frontend/lib/public/views/Overview/Overview.js
+++ frontend/lib/public/views/Overview/Overview.js
@@ -11,14 +11,28 @@ export default class Overview extends Observable {
     super();
     this.model = model;
     this.date = new Date().toDateString();
+    this.filterCriteria = [];
     this.data = [
       {
         authorID: "Batman",
         title: "Run1",
         creationTime: this.date,
         tags: ["Tag1", "Tag2"]
+      },
+      {
+        authorID: "Joker",
+        title: "Run2",
+        creationTime: this.date,
+        tags: ["Tag2"]
+      },
+      {
+        authorID: "Anonymous",
+        title: "Run5",
+        creationTime: this.date,
+        tags: ["Tag3"]
       }
     ];
+    this.filtered = [...this.data];
     this.headers = ["ID", "Author ID", "Title", "Creation Time"];
   }
   /**
@@ -28,8 +42,11 @@ export default class Overview extends Observable {
     return this.headers;
   };
 
+  /**
+   * @returns Array []
+   */
   getTableData = () => {
-    const subentries = this.data.map(entry => {
+    const subentries = this.filtered.map(entry => {
       const filter = Object.keys(entry).map(subkey => {
         if (subkey !== "tags") {
           return entry[subkey];
@@ -42,12 +59,56 @@ export default class Overview extends Observable {
     return subentries;
   };
 
+  addFilter = tag => {
+    this.filterCriteria = [...this.filterCriteria, tag];
+    this.getFilteredData();
+  };
+
+  removeFilter = condition => {
+    this.filterCriteria = this.filterCriteria.filter(tag => tag !== condition);
+    this.getFilteredData();
+  };
+
+  getFilteredData = () => {
+    this.filterCriteria.length !== 0
+      ? this.filterByTags()
+      : (this.filtered = [...this.data]);
+
+    this.notify();
+  };
+
+  filterByTags = () => {
+    this.filtered = this.data
+      .map(entry => {
+        let match = this.checkExistingTag(entry);
+
+        if (match) {
+          return entry;
+        }
+
+        return null;
+      })
+      .filter(entry => entry !== null);
+  };
+
+  checkExistingTag = entry => {
+    const check = this.filterCriteria.map(tag => {
+      const match = entry.tags.includes(tag);
+      if (match) {
+        return "Match";
+      }
+      return "No Match";
+    });
+
+    return check.includes("Match");
+  };
+
   getTagCounts = () => {
     return this.data.reduce((accumulator, currentValue) => {
       currentValue.tags.forEach(tag => {
-        accumulator[tag] = (accumulator[tag] || 0) + 1 
-      })
-      return accumulator
-    }, {})
-  }
+        accumulator[tag] = (accumulator[tag] || 0) + 1;
+      });
+      return accumulator;
+    }, {});
+  };
 }
